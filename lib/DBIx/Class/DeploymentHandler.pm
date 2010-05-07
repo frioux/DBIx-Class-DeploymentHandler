36821446c34719fc2db68bc53f18b4eef20d2aa7
Arthur Axel 'fREW' Schmidt <frioux@gmail.com>
2010-05-06 21:57:54 -0500

    whitespace

--- lib/DBIx/Class/DeploymentHandler.pm
+++ lib/DBIx/Class/DeploymentHandler.pm
@@ -45,8 +45,8 @@ sub install_version_storage {
 }
 
 sub prepare_install {
-   $_[0]->prepare_deploy;
-   $_[0]->prepare_version_storage_install;
+  $_[0]->prepare_deploy;
+  $_[0]->prepare_version_storage_install;
 }
 
 __PACKAGE__->meta->make_immutable;
