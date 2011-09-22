8b5b9ce4d0fdf0e01e7016d8f7ac8fe60f2b6279
John Napiorkowski <jjn1056@yahoo.com>
2011-09-22 08:55:20 -0400

    updated docs related to previous change in WithReasonableDefaults

--- lib/DBIx/Class/DeploymentHandler/WithReasonableDefaults.pm
+++ lib/DBIx/Class/DeploymentHandler/WithReasonableDefaults.pm
@@ -24,8 +24,8 @@ around prepare_downgrade => sub {
 
   my $args = shift || {};
 
-  $args->{to_version} ||= $self->database_version;
-  $args->{from_version}   ||= $self->schema_version;
+  $args->{to_version}   ||= $self->database_version;
+  $args->{from_version} ||= $self->schema_version;
   $args->{version_set}  ||= [$args->{from_version}, $args->{to_version}];
 
   $self->$orig($args);
@@ -65,9 +65,9 @@ Defaulted args:
 
 Defaulted args:
 
-  my $from_version = $self->database_version;
-  my $to_version   = $self->schema_version;
-  my $version_set  = [$to_version];
+  my $from_version = $self->schema_version;
+  my $to_version   = $self->data_version;
+  my $version_set  = [$from_version, $to_version];
 
 =head2 install_resultsource
 
