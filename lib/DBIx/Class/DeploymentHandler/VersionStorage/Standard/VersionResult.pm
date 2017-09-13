4c9f5ff2166524b1ef1c1640c9412f69ef96c68f
Andrew Gregory <andrew.gregory.8@gmail.com>
2017-09-13 10:31:25 -0400

    add hook to rename constraints
    Constraint names are set the moment add_unique_constraint is called.
    For users sub-classing to change the table name, this means that the
    index was still built with the original
    dbix_class_deploymenthandler_versions table name.  This resulted in
    a misleadingly named index and would prevent multiple versioned schemas
    from being installed to the same database because of the overlapping
    index names.

--- lib/DBIx/Class/DeploymentHandler/VersionStorage/Standard/VersionResult.pm
+++ lib/DBIx/Class/DeploymentHandler/VersionStorage/Standard/VersionResult.pm
@@ -7,7 +7,9 @@ use warnings;
 
 use parent 'DBIx::Class::Core';
 
-__PACKAGE__->table('dbix_class_deploymenthandler_versions');
+my $table = 'dbix_class_deploymenthandler_versions';
+
+__PACKAGE__->table($table);
 
 __PACKAGE__->add_columns (
   id => {
@@ -33,6 +35,17 @@ __PACKAGE__->add_columns (
 __PACKAGE__->set_primary_key('id');
 __PACKAGE__->add_unique_constraint(['version']);
 __PACKAGE__->resultset_class('DBIx::Class::DeploymentHandler::VersionStorage::Standard::VersionResultSet');
+
+sub sqlt_deploy_hook {
+  my ( $self, $sqlt_table ) = @_;
+  my $tname = $sqlt_table->name;
+  return if $tname eq $table;
+  # give indices unique names for sub-classes on different tables
+  foreach my $c ( $sqlt_table->get_constraints ) {
+    ( my $cname = $c->name ) =~ s/\Q$table\E/$tname/;
+    $c->name($cname);
+  }
+}
 
 1;
 
